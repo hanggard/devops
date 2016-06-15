d51075336fc4c1f5f3b9e2656f8f6d313fef5686
Elias Niczupienko <elias@veeqo.com>
2016-06-15 16:45:32 +0600

    Added example cookbook

---
+++ chef/example_cookbook/Readme.md
@@ -0,0 +1,1 @@
+Just create /var/chef/ directory and move example_cookbook to /var/chef/cookbooks
---
+++ chef/example_cookbook/utils/recipes/default.rb
@@ -0,0 +1,11 @@
+# Example of package installation in one string
+package [ "zip", "unzip", "git" ]
+
+# Example of separate package installations
+package "rsync" do
+  action :install
+end
+
+package "nfs-utils" do
+  action :install
+end
